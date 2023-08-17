669ec8788d784eca1dda2e0bb8ac023b39ee9fdf
123Masha456 <xmail99@yandex.ru>
2023-08-17 01:05:18 -0700

    TEST(employee and department)

---
+++ src/test/java/com.example.springlist/service/DepartmentServiceImplTest.java
@@ -0,0 +1,79 @@
+package com.example.springlist.service;
+
+import com.example.springlist.dto.Employee;
+import com.example.springlist.exceptions.EmployeeNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.*;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class DepartmentServiceImplTest {
+    @Mock
+    EmployeeService employeeService;
+    @InjectMocks
+    DepartmentServiceImpl underTest;
+
+    Employee ivan = new Employee("IVANOV", "IVAN", 1, 20000);
+    Employee petr = new Employee("PETROV", "PETR", 1, 30000);
+    Employee klara = new Employee("KLAROVA", "KLARA", 2, 40000);
+    Collection<Employee> employees;
+
+    @BeforeEach
+    void beforeEach() {
+        employees = List.of(ivan, petr, klara);
+    }
+
+    @Test
+    void findMaxSalaryEmployee_employeeFound_returnEmployeeWithMaxSalary() {
+        when(employeeService.findAll()).thenReturn(employees);
+        Employee result = underTest.findMaxSalaryEmployee(1);
+        assertEquals(petr, result);
+    }
+
+    @Test
+    void findMaxSalaryEmployee_employeeNotFound_throwEmployeeNotFoundException() {
+        when(employeeService.findAll()).thenReturn(Collections.emptyList());
+        EmployeeNotFoundException ex =
+                assertThrows(EmployeeNotFoundException.class,
+                        () -> underTest.findMaxSalaryEmployee(1));
+        assertEquals("Такой сотрудник не найден", ex.getMessage());
+    }
+
+    @Test
+    void findMinSalaryEmployee_employeeFound_returnEmployeeWithMinSalary() {
+        when(employeeService.findAll()).thenReturn(employees);
+        Employee result = underTest.findMinSalaryEmployee(1);
+        assertEquals(ivan, result);
+    }
+
+    @Test
+    void findMinSalaryEmployee_employeeNotFound_returnEmployeeWithMinSalary() {
+        when(employeeService.findAll()).thenReturn(Collections.emptyList());
+        EmployeeNotFoundException ex =
+                assertThrows(EmployeeNotFoundException.class,
+                        () -> underTest.findMinSalaryEmployee(1));
+        assertEquals("Такой сотрудник не найден", ex.getMessage());
+    }
+
+    @Test
+    void getAllEmployeesInDepartment_employeesFound_returnAllEmployeesInDepartment() {
+        when(employeeService.findAll()).thenReturn(employees);
+        Collection<Employee> result = underTest.getAll(1);
+        assertEquals(List.of(ivan, petr), result);
+    }
+
+    @Test
+    void getAllGroupingByDepartment_employeesFound_returnEmployeesInEachDepartment() {
+        when(employeeService.findAll()).thenReturn(employees);
+        Map<Integer, List<Employee>> result = underTest.getAllGroupingBy();
+        assertEquals(Map.of(1, List.of(ivan, petr), 2, klara), result);
+    }
+}
---
+++ src/test/java/com.example.springlist/service/EmployeeServiceImplTest.java
@@ -0,0 +1,101 @@
+package com.example.springlist.service;
+
+import com.example.springlist.dto.Employee;
+import com.example.springlist.exceptions.EmployeeAlreadyAddedException;
+import com.example.springlist.exceptions.EmployeeNotFoundException;
+import com.example.springlist.exceptions.EmployeeStorageIsFullException;
+import org.junit.jupiter.api.Test;
+
+
+import javax.lang.model.element.UnknownDirectiveException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeServiceImplTest {
+    EmployeeServiceImpl underTest = new EmployeeServiceImpl();
+    Employee employee = new Employee
+            ("IVANOV", "IVAN", 1, 40000);
+    Employee employee1 = new Employee
+            ("PETROV", "PETR", 1, 10000);
+    Employee employee2 = new Employee
+            ("ALEXEEV", "ALEX", 2, 20000);
+
+
+    @Test
+    void addEmployee_employeeNumberOverloaded_thrownException() {
+        underTest.addEmployee(employee.getLastName(), employee.getName(),
+                employee.getDepartment(), employee.getSalary());
+        underTest.addEmployee(employee1.getLastName(), employee1.getName(),
+                employee1.getDepartment(), employee1.getSalary());
+
+        EmployeeStorageIsFullException ex =
+                assertThrows(EmployeeStorageIsFullException.class,
+                        () -> underTest.addEmployee(employee2.getLastName(), employee2.getName(),
+                                employee2.getDepartment(), employee2.getSalary()));
+        assertEquals("Превышен лимит количества сотрудников", ex.getMessage());
+    }
+
+    @Test
+    void addEmployee_employeeIsAlreadyInMap_thrownException() {
+        underTest.addEmployee(employee2.getLastName(), employee2.getName(),
+                employee2.getDepartment(), employee2.getSalary());
+
+        EmployeeAlreadyAddedException ex =
+                assertThrows(EmployeeAlreadyAddedException.class,
+                        () -> underTest.addEmployee(employee2.getLastName(), employee2.getName(),
+                                employee2.getDepartment(), employee2.getSalary()));
+        assertEquals("В коллекции уже есть такой сотрудник", ex.getMessage());
+    }
+
+    @Test
+    void addEmployee_employeeAdded_employeeReturned() {
+        underTest.addEmployee(employee.getLastName(), employee.getName(),
+                employee.getDepartment(), employee.getSalary());
+        assertTrue(underTest.findAll().contains(employee));
+    }
+
+    @Test
+    void removeEmployee_employeeIsNotInMap_thrownException() {
+        EmployeeNotFoundException ex =
+                assertThrows(EmployeeNotFoundException.class,
+                        () -> underTest.removeEmployee(
+                                employee.getName(),
+                                employee.getLastName()));
+        assertEquals("Сотрудник не найден", ex.getMessage());
+    }
+
+    @Test
+    void removeEmployee_employeeIsInMap_employeeRemovedAndReturned() {
+        underTest.addEmployee(employee.getLastName(), employee.getName(),
+                employee.getDepartment(), employee.getSalary());
+        Employee result = underTest.removeEmployee(employee.getLastName(),
+                employee.getName());
+        assertEquals(employee, result);
+        assertFalse(underTest.findAll().contains(employee));
+    }
+
+    @Test
+    void findEmployee_employeeIsNotInMap_thrownException() {
+        EmployeeNotFoundException ex =
+                assertThrows(EmployeeNotFoundException.class,
+                        () -> underTest.findEmployee(
+                                employee.getName(),
+                                employee.getLastName()));
+        assertEquals("Сотрудник не найден", ex.getMessage());
+    }
+
+    @Test
+    void findEmployee_employeeIsInMap_employeeFoundAndReturned() {
+        underTest.addEmployee(employee.getLastName(), employee.getName(),
+                employee.getDepartment(), employee.getSalary());
+        Employee result = underTest.findEmployee(employee.getLastName(), employee.getName());
+        assertEquals(employee, result);
+    }
+
+    @Test
+    void findAll_allEmployeeInMap_returnedAllEmployees() {
+        underTest.addEmployee(employee.getLastName(), employee.getName(),
+                employee.getDepartment(), employee.getSalary());
+        assertTrue(underTest.findAll().contains(employee));
+    }
+}
